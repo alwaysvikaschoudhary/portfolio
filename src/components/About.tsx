58a1b0f746ed30a9a4e65a36d6ff69363cfd3397
Vikas Choudhary <alwaysvikaschoudhary@gmail.com>
2026-03-17 14:17:04 +0530

    made portfolio

---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+          Full Stack Developer and Software Engineer currently pursuing B.Tech in 
+          Information Technology at IIIT Una. Experienced in building scalable
+          full-stack applications using React.js and Spring Boot. Skilled in 
+          designing RESTful APIs, implementing secure authentication, and 
+          improving frontend performance. Passionate about AI-driven solutions 
+          and scalable architectures.
+      </div>
+    </div>
+  );
+};
+
+export default About;
