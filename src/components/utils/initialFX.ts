fb192140f595c95875a4c926fea98eb14c15a0a6
Vikas Choudhary <alwaysvikaschoudhary@gmail.com>
2026-03-30 00:30:00 +0530

    deploy

--- src/components/utils/initialFX.ts
+++ src/components/utils/initialFX.ts
@@ -1,10 +1,10 @@
-import { SplitText } from "gsap-trial/SplitText";
-import gsap from "gsap";
-import { smoother } from "../Navbar";
+
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+
 
 export function initialFX() {
   document.body.style.overflowY = "auto";
-  smoother.paused(false);
   document.getElementsByTagName("main")[0].classList.add("main-active");
   gsap.to("body", {
     backgroundColor: "#0a0e17",
